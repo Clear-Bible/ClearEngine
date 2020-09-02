e5a437fc5fcae9b0d686034459591301f43d85e2
Tim Sauerwein <timsauerwein@frontier.com>
2020-09-02 11:38:28 -0700

    Created solution file.  Added regression test with a stub.

---
+++ test/RegressionTest1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RegressionTest1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
