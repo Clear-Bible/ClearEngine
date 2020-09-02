1a105584daefccbe26052baaeb48f06a7658210d
Tim Sauerwein <timsauerwein@frontier.com>
2020-09-02 16:55:48 -0700

    Work in progress on regression test 1.

--- test/RegressionTest1/Program.cs
+++ test/RegressionTest1/Program.cs
@@ -16,25 +16,28 @@ namespace RegressionTest1
         {
             Console.WriteLine("Regression Test 1");
 
-            string rawPath = null;
-            string tokPath = null;
-            string lang = null;
-            ArrayList puncs = null;
+            string rawPath = "T/Verse.txt";
+            string tokPath = "T/target.punc.txt";
+            string lang = "Eggon";
+            ArrayList puncs = Data.GetWordList("puncs.txt");
 
             Console.WriteLine("Tokenizing");
             Tokens.Tokenize(rawPath, tokPath, puncs, lang);
 
-            ArrayList versificationList = null;
+            string versificationPath = "T/Versification.xml";
+            ArrayList versificationList =
+                Versification.LoadVersificationList(versificationPath,
+                "S1", "id");
 
-            string sourcePath = null;
-            string sourceIdPath = null;
-            string sourceIdLemmaPath = null;
-            string targetPath = null;
-            string parallelSourcePath = null;
-            string parallelSourceIdPath = null;
-            string parallelSourceIdLemmaPath = null;
-            string parallelTargetPath = null;
-            string parallelTargetIdPath = null;
+            string sourcePath = "S/source.txt";
+            string sourceIdPath = "S/source.id.txt";
+            string sourceIdLemmaPath = "S/source.id.lemma.txt";
+            string targetPath = "T/target.punc.txt";
+            string parallelSourcePath = "T/source.txt";
+            string parallelSourceIdPath = "T/source.id.txt";
+            string parallelSourceIdLemmaPath = "T/source.id.lemma.txt";
+            string parallelTargetPath = "T/target.txt";
+            string parallelTargetIdPath = "T/target.id.txt";
 
             Console.WriteLine("Creating Parallel Files");
             GroupVerses.CreateParallelFiles(
@@ -45,67 +48,72 @@ namespace RegressionTest1
                 parallelTargetPath, parallelTargetIdPath,
                 versificationList);
 
-            ArrayList sourceFuncWords = null;
-            ArrayList targetFuncWords = null;
-            string sourcePath2 = null;
-            string sourceIdPath2 = null;
-            string targetPath2 = null;
-            string targetIdPath = null;
-            string targetIdPath2 = null;
+            ArrayList sourceFuncWords = Data.GetWordList("sourceFuncWords.txt");
+            ArrayList targetFuncWords = Data.GetWordList("T/targetFuncWords.txt");
 
-            Data.FilterOutFunctionWords(sourcePath, sourcePath2, sourceFuncWords);
-            Data.FilterOutFunctionWords(sourceIdPath, sourceIdPath2, sourceFuncWords);
-            Data.FilterOutFunctionWords(targetPath, targetPath2, targetFuncWords);
-            Data.FilterOutFunctionWords(targetIdPath, targetIdPath2, targetFuncWords);
+            // source.txt target.txt source.id.txt target.id.txt 1:10;H:5 0.1 transModel.txt alignModel.txt
 
-            string runspec = null;
-            double epsilon = 0.0;
-            string transModelPath = null;
-            string alignModelPath = null;
+            string sourcePathA = "T/source.txt";
+            string sourcePath2 = "T/sourceFile.cw.txt";
+            string sourceIdPathA = "T/source.id.txt";
+            string sourceIdPath2 = "T/sourceFile.id.cw.txt";
+            string targetPathA = "T/target.txt";
+            string targetPath2 = "T/targetFile.cw.txt";
+            string targetIdPathA = "T/target.id.txt";
+            string targetIdPath2 = "T/targetFile.id.cw.txt";
+
+            Data.FilterOutFunctionWords(sourcePathA, sourcePath2, sourceFuncWords);
+            Data.FilterOutFunctionWords(sourceIdPathA, sourceIdPath2, sourceFuncWords);
+            Data.FilterOutFunctionWords(targetPathA, targetPath2, targetFuncWords);
+            Data.FilterOutFunctionWords(targetIdPathA, targetIdPath2, targetFuncWords);
+
+            string transModelPath = "T/transModel.txt";
+            string alignModelPath = "T/alignModel.txt";
 
             Console.WriteLine("Building Models");
             BuildTransModels.BuildModels(
                 sourcePath2, targetPath2, sourceIdPath2, targetIdPath2,
-                runspec, epsilon,
+                "T/1:10;H:5", 0.1,
                 transModelPath, alignModelPath);
 
             Hashtable bookNames = BookTables.LoadBookNames3();
 
-            string jsonOutput = null;
+            string jsonOutput = "T/alignment.json";
 
-            Hashtable transModel = null;
-            Hashtable manTransModel = null;
-            string treeFolder = null;
-            Hashtable alignProbs = null;
-            Hashtable preAlignment = null;
+            Hashtable transModel = Data.GetTranslationModel("T/transModel.txt");
+            Hashtable manTransModel = Data.GetTranslationModel2("T/manTransModel.txt");
+            Hashtable alignProbs = Data.GetAlignmentModel("alignModel.txt");
+            Hashtable preAlignment = Data.BuildPreAlignmentTable(alignProbs);
             bool useAlignModel = true;
-            Hashtable groups = null;
-            ArrayList stopWords = null;
-            Hashtable goodLinks = null;
-            int goodLinkMinCount = 0;
-            Hashtable badLinks = null;
-            int badLinkMinCount = 0;
-            Hashtable glossTable = null;
-            Hashtable oldLinks = null;
+            int maxPaths = 1000000;
+            Hashtable groups = Data.LoadGroups("T/groups.txt");
+            ArrayList stopWords = Data.GetStopWords("T/stopWords.txt");
+            Hashtable goodLinks = Data.GetXLinks("T/goodLinks.txt");
+            int goodLinkMinCount = 3;
+            Hashtable badLinks = Data.GetXLinks("T/badLinks.txt");
+            int badLinkMinCount = 3;
+            Hashtable glossTable = Data.BuildGlossTableFromFile("Gloss.txt");
+            Hashtable oldLinks = Data.GetOldLinks("T/oldAlignment.json", ref groups);
             bool contentWordsOnly = true;
-            Hashtable strongs = null;
+            Hashtable strongs = Data.BuildStrongTable("T/strongs.txt");
 
             Console.WriteLine("Auto Alignment");
             AutoAligner.AutoAlign(
-                sourceIdPath, sourceIdLemmaPath, targetIdPath,
+                "T/source.id.txt", "T/source.id.lemma.txt",
+                "T/target.id.txt",
                 jsonOutput,
                 transModel, manTransModel,
-                treeFolder,
+                "Trees",
                 bookNames,
                 alignProbs, preAlignment, useAlignModel,
-                1000000,
+                maxPaths,
                 puncs, groups, stopWords,
                 goodLinks, goodLinkMinCount, badLinks, badLinkMinCount,
                 glossTable,
                 oldLinks,
                 sourceFuncWords, targetFuncWords, contentWordsOnly, strongs);
 
-            string jsonOutputRef = null;
+            string jsonOutputRef = "Compare/alignment.json";
 
             Console.WriteLine("Comparing JSON Output Files");
             if (FilesMatch(jsonOutput, jsonOutputRef))
