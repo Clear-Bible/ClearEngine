8f680ebf49dd46a8d7558de7b4858d78ec4706ef
Tim Sauerwein <timsauerwein@frontier.com>
2020-10-06 13:55:54 -0700

    Added some rendering-as-string utilities for debugging. This version compiles and appears to execute correctly.

--- src/GBI_Aligner/TimUtil.cs
+++ src/GBI_Aligner/TimUtil.cs
@@ -1,15 +1,47 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Xml;
 
 using Newtonsoft.Json;
+
+using Utilities;
 
 
 namespace GBI_Aligner
 {
     class TimUtil
     {
+        public static string DebugTreeToString(XmlNode xmlNode)
+        {
+            if (xmlNode.FirstChild.NodeType.ToString() == "Text") // terminal node
+            {
+                return $"[{xmlNode.InnerText}] ";
+            }
+            else
+            {
+                return "(" + String.Concat(xmlNode.ChildNodes
+                    .Cast<XmlNode>()
+                    .Select(n => DebugTreeToString(n)))
+                    + " )";
+            }
+        }
+
+        public static string DebugAlignmentsToString(Hashtable alignments)
+        {
+            string renderAlignments(object alignments) =>
+                "(" +
+                String.Concat(((ArrayList)alignments)
+                    .Cast<object>()
+                    .Select(o => o.GetType().ToString() + " ")) + " )";
+
+            return String.Concat(alignments
+                .Cast<DictionaryEntry>()
+                .Select(kvp => $"({kvp.Key} {renderAlignments(kvp.Value)} "));
+        }
+
         public static void PrintArrayList(string name, ArrayList arrayList)
         {
             using (StringWriter sw = new StringWriter())
