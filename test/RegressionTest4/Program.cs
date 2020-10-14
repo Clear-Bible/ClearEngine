7c6a82bea85c30c4140bbb54297b9740bc63c2b9
Tim Sauerwein <timsauerwein@frontier.com>
2020-10-14 10:56:47 -0700

    Added a WorkInProgressStaging project. Added experiments with immutable and mutable versions of a TranslationScores datatype.

--- test/RegressionTest4/Program.cs
+++ test/RegressionTest4/Program.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 
 using GBI_Aligner;
 using AlignmentTool;
+
+using WorkInProgressStaging;
 
 using ClearBible.Clear3.InternalDb;
 
@@ -36,25 +39,56 @@ namespace RegressionTest4
             Dictionary<string, Dictionary<string, double>> transModel =
                 Data.GetTranslationModel(transModelPath);
 
-            foreach (string s in
-                transModel.Take(10).SelectMany(kvp =>
-                    kvp.Value.Select(kvp2 => $"{kvp.Key} {kvp2.Key} {kvp2.Value}")))
+            Stopwatch watch = Stopwatch.StartNew();
+
+            TranslationScores scores = TranslationScores.Empty;
+
+            foreach (var x in
+                transModel.SelectMany(kvp =>
+                    kvp.Value.Select(kvp2 => Tuple.Create(kvp.Key, kvp2.Key, kvp2.Value))))
             {
-                Console.WriteLine(s);
+                TranslationScore s = new TranslationScore(x.Item1, x.Item2, x.Item3);
+                scores = scores.Add(s);
             }
-            Console.WriteLine();
 
-            string subject = "xyzzy314159";
-            string hash = DbUtility.Hash(subject);
-            Console.WriteLine($"{subject} -> {hash}");
+            watch.Stop();
 
-            string k1(string s) => DbUtility.MakeKey(s);
-            string k2(string[] ks) => DbUtility.MakeKey(ks);
+            Console.WriteLine($"sources: {scores.AllSources.Count()}");
+            Console.WriteLine($"targets: {scores.AllTargets.Count()}");
+            Console.WriteLine($"milliseconds: {watch.ElapsedMilliseconds}");
 
-            string key1 = k2(new string[] { k1("abc"), k1("def") });
-            string key2 = k2(new string[] { k1("abcd"), k1("ef") });
-            Console.WriteLine(key1);
-            Console.WriteLine(key2);
+            watch.Reset();
+            watch.Start();
+
+            TranslationScores2 scores2 = new TranslationScores2();
+
+            foreach (var x in
+                transModel.SelectMany(kvp =>
+                    kvp.Value.Select(kvp2 => Tuple.Create(kvp.Key, kvp2.Key, kvp2.Value))))
+            {
+                TranslationScore s = new TranslationScore(x.Item1, x.Item2, x.Item3);
+                scores2.Add(s);
+            }
+
+
+            watch.Stop();
+
+            Console.WriteLine("scores2:");
+            Console.WriteLine($"sources: {scores2.AllSources.Count()}");
+            Console.WriteLine($"targets: {scores2.AllTargets.Count()}");
+            Console.WriteLine($"milliseconds: {watch.ElapsedMilliseconds}");
+
+
+            //foreach (string s in scores.AllTargets)
+            //{
+            //    Console.WriteLine(s);
+            //    foreach (TranslationScore score in scores.Sources(s))
+            //    {
+            //        Console.WriteLine($"   {score}");
+            //    }
+            //    double total = scores.Sources(s).Sum(score => score.Score);
+            //    Console.WriteLine($"   TOTAL: {total}");
+            //}           
         }
     }
 
