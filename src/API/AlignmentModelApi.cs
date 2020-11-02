1640c6ff8c66538597ddd6e68a1e6cda64f86de9
Tim Sauerwein <timsauerwein@frontier.com>
2020-11-02 13:50:14 -0800

    Defined new interface type AlignmentModel, created ImportAlignmentModel() to read it in.

---
+++ src/API/AlignmentModelApi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClearBible.Clear3.API
+{
+    public class AlignmentModel
+    {
+        public Dictionary<Tuple<SourceID, TargetID>, Score> Inner;
+
+        public AlignmentModel(
+            Dictionary<Tuple<SourceID, TargetID>, Score> inner)
+        {
+            Inner = inner;
+        }
+    }
+}
