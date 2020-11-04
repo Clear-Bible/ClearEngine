647e1a7e93d7636c7bf016dd45eee4549f464426
Tim Sauerwein <timsauerwein@frontier.com>
2020-11-04 13:10:39 -0800

    More cleanup in TreeService.

--- src/Impl.TreeService/TreeService.cs
+++ src/Impl.TreeService/TreeService.cs
@@ -19,18 +19,14 @@ namespace ClearBible.Clear3.Impl.TreeService
 
     public class TreeService : ITreeService
     {
-        public void GetChapterTree(
-            ChapterID chapterID,
-            string treeFolder,
-            Dictionary<string, XmlNode> trees,
-            Dictionary<string, string> bookNames)
+        public void GetChapterTree(ChapterID chapterID)
         {
             int bookNumber = chapterID.Book;           
             int chapterNumber = chapterID.Chapter;
 
-            string bookName = (string)bookNames[$"{bookNumber:D2}"];
+            string bookName = _bookNames[$"{bookNumber:D2}"];
 
-            string treeFile = Path.Combine(treeFolder, $"{bookName}{chapterNumber:D3}.trees.xml");
+            string treeFile = Path.Combine(_treeFolder, $"{bookName}{chapterNumber:D3}.trees.xml");
 
             if (File.Exists(treeFile))
             {
@@ -40,35 +36,17 @@ namespace ClearBible.Clear3.Impl.TreeService
                     .Descendants("Sentence")
                     .Select(s => s.Descendants("Node").First()))
                 {
-                    string verseId =
+                    string verseIdString =
                         node.Attribute("nodeId").Value.Substring(0, 8);
-                    _trees2[verseId] = node;
-                }
-
-                XmlDocument xmlDoc = new XmlDocument();
-                xmlDoc.Load(treeFile);
-
-                XmlNodeList sentences = xmlDoc.SelectNodes("Sentences/Sentence");
-
-                foreach (XmlNode sentence in sentences)
-                {
-                    XmlNode treeNode = sentence.FirstChild.FirstChild.FirstChild;
-
-                    string verseID = Utils.GetAttribValue(treeNode, "nodeId").Substring(0, 8);
-
-                    trees.Add(verseID, treeNode);
+                    VerseID verseID = new VerseID(verseIdString);
+                    _trees2[verseID] = node;
                 }
             }
         }
 
 
-        // FIXME would prefer to use newer Linq-to-XML library
-        //
-        private Dictionary<string, XmlNode> _trees =
-            new Dictionary<string, XmlNode>();
-
-        private Dictionary<string, XElement> _trees2 =
-            new Dictionary<string, XElement>();
+        private Dictionary<VerseID, XElement> _trees2 =
+            new Dictionary<VerseID, XElement>();
 
         private ChapterID _currentChapterID = ChapterID.None;
 
@@ -92,88 +70,93 @@ namespace ClearBible.Clear3.Impl.TreeService
         {
             if (!chapterID.Equals(_currentChapterID))
             {
-                _trees.Clear();
                 _trees2.Clear();
-                // Get the trees for the current chapter; a verse can cross chapter boundaries
 
-                GetChapterTree(chapterID, _treeFolder, _trees, _bookNames);
+                // Get the trees for the current chapter; a verse can cross chapter boundaries
+                GetChapterTree(chapterID);
                 int book = chapterID.Book;
                 int chapter = chapterID.Chapter;
                 ChapterID prevChapterID = new ChapterID(book, chapter - 1);
-                GetChapterTree(prevChapterID, _treeFolder, _trees, _bookNames);
+                GetChapterTree(prevChapterID);
                 ChapterID nextChapterID = new ChapterID(book, chapter + 1);
-                GetChapterTree(nextChapterID, _treeFolder, _trees, _bookNames);
+                GetChapterTree(nextChapterID);
                 _currentChapterID = chapterID;
             }
         }
 
-        public Dictionary<string, XmlNode> Legacy => _trees;
 
         public XmlNode GetTreeNode(VerseID start, VerseID end)
         {
-            string sStartVerseID = $"{start.Book:D2}{start.Chapter:D3}{start.Verse:D3}";
+            List<XElement> verseTrees = GetVerseTrees(start, end).ToList();
 
-            string sEndVerseID = $"{end.Book:D2}{end.Chapter:D3}{end.Verse:D3}";
-
-            List<XElement> subTrees = GetSubTrees(start, end);
-
-            if (subTrees.Count == 1)
+            if (verseTrees.Count == 1)
             {
-                return subTrees[0].ToXmlNode();
+                return verseTrees[0].ToXmlNode();
             }
             else
             {
-                return CombineTrees(subTrees).ToXmlNode();
+                return CombineTrees(verseTrees).ToXmlNode();
             }
         }
 
-        List<XElement> GetSubTrees(VerseID start, VerseID end)
-        {
-            List<XElement> subTrees = new List<XElement>();
 
+        IEnumerable<XElement> GetVerseTrees(VerseID start, VerseID end)
+        {
             int book = start.Book;
-            int startChapter = start.Chapter;
-            int endChapter = end.Chapter;
 
-            if (startChapter == endChapter)
+            if (start.Chapter == end.Chapter)
             {
-                GetSubTreesInSameChapter(start, end, book, startChapter, subTrees);
+                return GetChapterSubrange(book, start.Chapter, start.Verse, end.Verse);
             }
             else
             {
-                GetSubTreesInDiffChapter(start, end, book, startChapter, endChapter, ref subTrees);
+                return
+                    GetChapterTail(book, start.Chapter, start.Verse)
+                    .Concat(GetChapterSubrange(book, end.Chapter, 1, end.Verse));
             }
-
-            return subTrees;
         }
 
-        void GetSubTreesInSameChapter(VerseID sStartVerseID, VerseID sEndVerseID, int book, int chapter, List<XElement> subTrees)
-        {
-            int startVerse = sStartVerseID.Verse;
-            int endVerse = sEndVerseID.Verse;
 
+        IEnumerable<XElement> GetChapterSubrange(
+            int book,
+            int chapter,
+            int startVerse,
+            int endVerse)
+        {
             for (int i = startVerse; i <= endVerse; i++)
             {
-                string verseID = $"{book:D2}{chapter:D3}{i:D3}";
-
-                if (_trees2.TryGetValue(verseID, out XElement subTree))
+                if (_trees2.TryGetValue(
+                    new VerseID(book, chapter, i),
+                    out XElement subTree))
                 {
-                    subTrees.Add(subTree);
+                    yield return subTree;
                 }
                 else
                 {
-                    break;
+                    yield break;
                 }
             }
         }
 
-        void GetSubTreesInDiffChapter(VerseID sStartVerseID, VerseID sEndVerseID, int book, int chapter1, int chapter2, ref List<XElement> subTrees)
-        {
-            VerseID hypotheticalLastVerse = new VerseID(book, chapter1, 100);           
-            GetSubTreesInSameChapter(sStartVerseID, hypotheticalLastVerse, book, chapter1, subTrees);
 
-            VerseID hypotheticalFirstVerse = new VerseID(book, chapter2, 1);
-            GetSubTreesInSameChapter(hypotheticalFirstVerse, sEndVerseID, book, chapter2, subTrees);
+        IEnumerable<XElement> GetChapterTail(
+            int book,
+            int chapter,
+            int startVerse)
+        {
+            for (int i = startVerse; true; i++)
+            {
+                if (_trees2.TryGetValue(
+                    new VerseID(book, chapter, i),
+                    out XElement subTree))
+                {
+                    yield return subTree;
+                }
+                else
+                {
+                    yield break;
+                }
+            }
         }
 
 
