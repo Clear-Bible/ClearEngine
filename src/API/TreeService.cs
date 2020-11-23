97e8903d7cdf4194522965645bada5a826d5d6d4
Tim Sauerwein <timsauerwein@frontier.com>
2020-11-23 15:57:34 -0800

    Moving code around and removing obsolete code.  Work in progress checkpoint.

--- src/API/TreeService.cs
+++ src/API/TreeService.cs
@@ -3,23 +3,12 @@ using System.Collections.Generic;
 
 namespace ClearBible.Clear3.API
 {
-    public interface ITreeService_Old
-    {
-        Uri IdUri { get; }
-
-        Corpus Corpus { get; }
-
-        string GetLemma(IPlace place);
-
-        string GetStrong(IPlace place);
-
-        string GetPartOfSpeech(IPlace place);
-
-        string GetMorphology(IPlace place);
-
-        long GetLegacyID(IPlace place);
-    }
-
+    
+    /// <summary>
+    /// As far as the API is concerned, a tree service is a completely
+    /// abstract datum right now.
+    /// </summary>
+    /// 
     public interface ITreeService
     {
 
