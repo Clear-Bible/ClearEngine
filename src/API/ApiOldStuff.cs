b8f3f863290c23ed448e976195942abadc6fd45f
Tim Sauerwein <timsauerwein@frontier.com>
2020-11-23 15:06:53 -0800

    Removing obsolete code and rearranging, work in progress checkpoint.

---
+++ src/API/ApiOldStuff.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ClearBible.Clear3.API
+{
+    // Old stuff that I want to get rid of.
+
+
+    public interface ITranslationModel
+    {
+        void AddEntry(
+            string sourceLemma,
+            string targetMorph,
+            double score);
+    }
+}
