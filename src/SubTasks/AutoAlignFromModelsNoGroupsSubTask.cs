3e368df4ac222b5309cf0bd0b6a93705c88191d4
Tim Sauerwein <timsauerwein@frontier.com>
2020-11-24 15:54:15 -0800

    Work in progress checkpoint for AutoAlignFrmModelsNoGroups subtask.

--- src/SubTasks/AutoAlignFromModelsNoGroupsSubTask.cs
+++ src/SubTasks/AutoAlignFromModelsNoGroupsSubTask.cs
@@ -4,14 +4,50 @@ using System.Linq;
 
 namespace ClearBible.Clear3.SubTasks
 {
+    using System.Net.Http.Headers;
     using ClearBible.Clear3.API;
 
     public class AutoAlignFromModelsNoGroupsSubTask
     {
         public static Alignment2 Run(
-            )
+            List<TranslationPair> translationPairs,
+            ITreeService treeService,
+            Dictionary<string, Gloss> glossTable,
+            IAutoAlignAssumptions assumptions)
         {
-            throw new NotImplementedException();
+            // This map of group key to position of primary
+            // word within group is required for output; just
+            // use an empty Dictionary.
+            Dictionary<string, int> primaryPositions =
+                new Dictionary<string, int>();
+
+            //Alignment2 align = new Alignment2()
+            //{
+            //    Lines =
+            //        translationPairs
+            //        .Select(translationPair =>
+            //        {
+            //            ZoneMonoAlignment zoneMonoAlignment =
+            //                AlignZone(
+            //                    treeService,
+            //                    translationPair,
+            //                    assumptions);
+
+            //            ZoneMultiAlignment zoneMultiAlignment =
+            //                ConvertToZoneMultiAlignment(zoneMonoAlignment);
+
+            //            return
+            //                Output.GetLine(
+            //                    zoneMultiAlignment,
+            //                    glossTable,
+            //                    primaryPositions);
+            //        })
+            //        .ToArray()
+            //};
+
+            //return align;
+
+            throw new NotImplementedException(); 
         }
     }
 }
