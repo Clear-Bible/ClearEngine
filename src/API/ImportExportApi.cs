8cc05cf9418e598c1fdd3098b0063b7b34b8c883
Tim Sauerwein <timsauerwein@frontier.com>
2020-11-25 08:06:54 -0800

    Moving import/export service into the regular API, work in progress checkpoint.

---
+++ src/API/ImportExportApi.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClearBible.Clear3.API
+{
+    public interface IImportExportService
+    {
+        TranslationModel ImportTranslationModel(
+            string filePath);
+
+        AlignmentModel ImportAlignmentModel(
+            string filePath);
+
+        GroupTranslationsTable ImportGroupTranslationsTable(
+            string filePath);
+
+        List<TranslationPair> ImportTranslationPairsFromLegacy(
+            string parallelSourcePath,
+            string parallelTargetPath);
+    }
+}
