436def24c9161871e2d5d6c80ba12af61118a4cf
Tim Sauerwein <timsauerwein@frontier.com>
2020-12-01 14:05:52 -0800

    Added documentation to API.

--- src/API/DataModel_SourceTargetIDs.cs
+++ src/API/DataModel_SourceTargetIDs.cs
@@ -3,13 +3,31 @@
 
 namespace ClearBible.Clear3.API
 {
-    // Data Model for Source and Target IDs
-    //-------------------------------------
-
-    // Ways of identifying locations in source and target corpora
-    // by book, chapter, verse, word, and (in case of a source) subsegment.
+    // Data Model: IDs for Source Segments, Target Words, Chapters, Verses
+    //--------------------------------------------------------------------
 
 
+    /// <summary>
+    /// <para>
+    /// Identifies a particular segment instance in the source manuscript,
+    /// as described by its book, chapter, verse, word, and subsegment numbers
+    /// as known to the syntax tree.
+    /// </para>
+    /// <para>
+    /// The SourceID object consists of a struct containing a single string
+    /// that encodes the identification in a canonical form.  This string is
+    /// available using the AsCanonicalString property.  A new SourceID can
+    /// be created from a canonical string if desired.
+    /// </para>
+    /// <para>
+    /// Once created, a SourceID is immutable.  Two SourceID objects are
+    /// Equal() if they contain the same canonical string, and will work as
+    /// dictionary keys on this basis.
+    /// </para>
+    /// </summary>
+    /// FIXME: Finish using SourceIDs instead of bare strings in the
+    /// system at large.
+    /// 
     public readonly struct SourceID
     {
         public int Book => int.Parse(_tag.Substring(0, 2));
@@ -35,6 +53,27 @@ namespace ClearBible.Clear3.API
     }
 
 
+    /// <summary>
+    /// <para>
+    /// Identifies a particular translated word instance in the translation,
+    /// as described by its book, chapter, verse, and word, numbers
+    /// as known to the translation.
+    /// </para>
+    /// <para>
+    /// The TargetID object consists of a struct containing a single string
+    /// that encodes the identification in a canonical form.  This string is
+    /// available using the AsCanonicalString property.  A new TargetID can
+    /// be created from a canonical string if desired.
+    /// </para>
+    /// <para>
+    /// Once created, a TargetID is immutable.  Two TargetID objects are
+    /// Equal() if they contain the same canonical string, and will work as
+    /// dictionary keys on this basis.
+    /// </para>
+    /// </summary>
+    /// FIXME: Finish using TargetIDs instead of bare strings in the
+    /// system at large.
+    /// 
     public readonly struct TargetID
     {
         public int Book => int.Parse(_tag.Substring(0, 2));
@@ -66,6 +105,23 @@ namespace ClearBible.Clear3.API
     }
 
 
+    /// <summary>
+    /// <para>
+    /// Identifies a chapter as described by its book and chapter numbers.
+    /// </para>
+    /// <para>
+    /// The ChapterID object consists of a struct containing a single string
+    /// that encodes the identification in a canonical form.  This string is
+    /// available using the AsCanonicalString property.  A new ChapterID can
+    /// be created from a canonical string if desired.
+    /// </para>
+    /// <para>
+    /// Once created, a ChapterID is immutable.  Two ChapterID objects are
+    /// Equal() if they contain the same canonical string, and will work as
+    /// dictionary keys on this basis.
+    /// </para>
+    /// </summary>
+    /// 
     public readonly struct ChapterID
     {
         public int Book => int.Parse(_tag.Substring(0, 2));
@@ -81,10 +137,35 @@ namespace ClearBible.Clear3.API
             _tag = $"{book:D2}{chapter:D3}";
         }
 
+        public string AsCanonicalString => _tag;
+
+        /// <summary>
+        /// A special value of ChapterID that has book 0 and chapter 0,
+        /// and that means "no chapter".
+        /// </summary>
+        /// 
         public static ChapterID None => new ChapterID("00000");
     }
 
 
+    /// <summary>
+    /// <para>
+    /// Identifies a particular verse as described by its book, chapter,
+    /// and verse numbers.
+    /// </para>
+    /// <para>
+    /// The VerseID object consists of a struct containing a single string
+    /// that encodes the identification in a canonical form.  This string is
+    /// available using the AsCanonicalString property.  A new VerseID can
+    /// be created from a canonical string if desired.
+    /// </para>
+    /// <para>
+    /// Once created, a VerseID is immutable.  Two VerseID objects are
+    /// Equal() if they contain the same canonical string, and will work as
+    /// dictionary keys on this basis.
+    /// </para>
+    /// </summary>
+    /// 
     public readonly struct VerseID
     {
         public int Book => int.Parse(_tag.Substring(0, 2));
