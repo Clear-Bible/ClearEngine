88a661007a452968de8592deefdf0560aa6534d4
Tim Sauerwein <timsauerwein@frontier.com>
2020-12-01 17:38:21 -0800

    More documentation for the API.

--- src/API/API_Miscellaneous.cs
+++ src/API/API_Miscellaneous.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 
 namespace ClearBible.Clear3.API
 {
+    /// <summary>
+    /// An exception that Clear3 services can throw.
+    /// </summary>
     public class ClearException : Exception
     {
         public ClearException(
@@ -26,6 +29,10 @@ namespace ClearBible.Clear3.API
     }
 
 
+    /// <summary>
+    /// Status codes for use in a ClearException.
+    /// </summary>
+    /// 
     public enum StatusCode
     {
         OK,
@@ -38,6 +45,11 @@ namespace ClearBible.Clear3.API
     }
 
 
+    /// <summary>
+    /// Progress report to be sent periodically from a Clear job that
+    /// is executing concurrently.
+    /// </summary>
+    /// 
     public interface ProgressReport
     {
         string Message { get; }
@@ -46,6 +58,27 @@ namespace ClearBible.Clear3.API
     }
 
 
+    /// <summary>
+    /// The type of a function that attempts to look up a value
+    /// for a given key.
+    /// </summary>
+    /// <typeparam name="TKey">
+    /// The type of the key.
+    /// </typeparam>
+    /// <typeparam name="TValue">
+    /// The type of the value.
+    /// </typeparam>
+    /// <param name="key">
+    /// The key that is sought.
+    /// </param>
+    /// <param name="value">
+    /// Will be set to the value found for the key, or to the default
+    /// value for its type if the key cannot be found.
+    /// </param>
+    /// <returns>
+    /// True if a value for the key was found, and false otherwise.
+    /// </returns>
+    /// 
     public delegate bool TryGet<TKey, TValue>(
         TKey key,
         out TValue value);
